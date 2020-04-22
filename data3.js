ce3d830676cb1120f4d0a9d627b014e726675223
jensenbgb-khalid-salim <59868570+jensenbgb-khalid-salim@users.noreply.github.com>
2020-04-22 14:33:36 +0200

    Add files via upload

---
+++ data3.js
@@ -0,0 +1,112 @@
+
+
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+app.use(bodyParser.json())
+
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+
+const adapter = new FileSync("db.json");
+const db = low (adapter);
+
+
+
+
+// Set some defaults (required if your JSON file is empty)
+db.defaults({ products: [], count: 0, carts: [] }).write();
+
+console.log('Using read(): ', db.read().value());
+console.log('Using get(): ', db.get('products').value());
+
+
+/*
+db.get("carts"). push({id:1, type: "sskor", price: 2000, company :"Nike"}).write();
+db.get("carts"). push({id:2, type: "T-shirt", price: 700, company :"Azar"}).write();
+db.get("carts"). push({id:3, type: "Strumpor", price: 900, company :"Addidas"}).write();
+
+db.get("products"). push({id:4, type: "Byxor", price: 500, company :"Team-mate"}).write();
+db.get("products"). push({id:5, type: "Underkläder", price: 100, company :"Acises"}).write(); */
+
+app.get(`/api/products/getAll`, (req,res) => {
+
+
+  AllProducts = db.get(`products`).value();
+  res.send(AllProducts);
+  console.log(AllProducts)
+});
+
+
+app.post(`/api/products/carts/:id`, (req,res) =>  {
+let TheProoduct = db.get(`products`).find ({id:parseInt(req.params.id)}).value();
+
+if (!TheProoduct) return res.status(404).send(" the proudect is not existing");
+
+let proudectExist= db.get(`carts`).find ({id:parseInt(req.params.id)}).value();
+
+if (proudectExist){
+
+  res.send("proudect are alredy existing")
+}
+else {
+
+  db.get("carts").push(TheProoduct).write();
+  console.log ("this proudect id selected:",TheProoduct);
+
+
+}
+
+res.send(JSON.stringify(TheProoduct));
+console.log(TheProoduct);
+
+
+})
+
+
+         
+
+app.get(`/api/carts/getAll`, (req,res) => {
+
+
+  const Allcarts = db.get(`carts`).value();
+  res.send(Allcarts);
+  console.log(Allcarts)
+});
+      
+
+
+app.delete(`/api/carts/delete/:id`, (req,res) => {
+
+
+ let deletecart = db.get(`carts`).remove({id:parseInt(req.params.id)}).write();
+ 
+ if (deletecart){
+
+  res.send("proudect are deleted", deletecart)
+}
+else {
+
+
+
+  console.log ("this proudect is alredy have been deleted:",deletecart);
+
+
+}
+
+
+  res.send(deletecart);
+  console.log(deletecart)
+
+
+});
+
+
+
+
+     app.get('/', (req, res) => res.send('Hello'))
+
+
+const port = 3000
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`)) 
